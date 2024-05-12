7cc0debf7fa40b47aa96d9e53101523d1383d271
Nishesh Gautam <mynishesh@gmail.com>
2024-05-12 16:56:33 +0545

    basic of text, function

--- App.tsx
+++ App.tsx
@@ -1,37 +1,52 @@
-
-
 import React from 'react';
+import { Button, Text, View } from 'react-native';
+import UserData from './components/usercomponent';
+/*
+above to import UserData no need of {} because we use keyword
+called default
+function ram(){
+  return "kasto hello";
+}
+*/
 
-import {
-
-
-  Text,
-  View,
-
-
-} from 'react-native';
 
 
 function App(): React.JSX.Element {
 
+  let s = "rama";
+
+  const ram=(val:string)=>
+   
 
 
-      /*
-      App(): React.JSX.Element-- meaning function App return JSX element which is ui of any app,you can
-      see App function donot take any argument since, App()--is void
+  { 
+    val = s; 
+    return console.warn(val);}
 
-  below {{fontSize:60}}--first {}outer represent js expression, inner {} represnt for style
+  
+
+  /*
+  -to master any programming language make a habit of 1. reading error then resolve
+   2. Do not make same mistake again once you learn something that should be in your
+   head permanently(by using that again and agian in your code)
+  App(): React.JSX.Element-- meaning function App return JSX element which is ui of any app,you can
+  see App function donot take any argument since, App()--is void
+
+below {{fontSize:60}}--first {}outer represent js expression, inner {} represnt for style
 */
-
-
   return (
     <View>
+     
       
-      <Text style={{fontSize:20}}>Hello handsomeee</Text>
-
+      <Text style={{ fontSize:35 }}>K chha</Text>
+      <Button
+      title='press me' color={'black'} onPress={ ()=>  ram('no hello')}></Button>
+      <Text>
+        hello
+      </Text>
+      <Button
+      title='press me2'></Button>
     </View>
-
-
   );
 }
 
---
+++ components/usercomponent.js
@@ -0,0 +1,25 @@
+import {Text, View} from 'react-native';
+
+//making componets
+var UserData = ()=>{
+    return(
+
+        <View>
+              <Text>Name: Ram</Text>
+              <Text>age: 29</Text>
+              <Text>roll no: 36</Text>
+              
+        </View>
+    
+      
+  
+  
+    );
+  
+  }
+  export default UserData;
+  /*
+ if you use default you do not have to use curly braces whenever
+ you want to import it
+
+  */
